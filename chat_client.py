0e34320394f8bd165d24e6ce90b5a661264967c0
JpryHyrd <42839731+JpryHyrd@users.noreply.github.com>
2019-05-30 15:22:20 +0300

    lesson_8
    nd

--- chat_client.py
+++ chat_client.py
@@ -1,16 +1,24 @@
 import select
+from threading import Thread, Lock
+import time
 from socket import socket, AF_INET, SOCK_STREAM
+
+t = Thread(target="chat")
+done = Lock()
 
 def read():
     try:
         data = socket.recv(1024).decode("ascii")
         print(data)
     except:
+        time.sleep(1)
         pass
 
 def write():
     msg = input("Cooбщение: ")
     socket.send(msg.encode("ascii"))
+    time.sleep(1)
+
 
 
 s = socket(AF_INET, SOCK_STREAM)
@@ -27,7 +35,9 @@ while True:
         write()
     else:
         continue
-
+if __name__ == "__main__":
+    t.daemon = True
+    t.start()
 
 
 
